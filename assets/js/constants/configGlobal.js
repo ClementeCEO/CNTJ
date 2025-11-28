46de36aa9a70dd82ce6689d301790913d0491e7f
ClementeCEO <joeltumbajulca515@gmail.com>
2025-11-28 15:29:36 -0500

    Update configGlobal.js

--- assets/js/constants/configGlobal.js
+++ assets/js/constants/configGlobal.js
@@ -1,5 +1,5 @@
-export const URL_JSON_CANALES_PRINCIPAL = 'https://cdn.jsdelivr.net/gh/Alplox/json-teles@refs/heads/main/canales.json';
-export const TWITCH_PARENT = 'alplox.github.io'
+export const URL_JSON_CANALES_PRINCIPAL = 'https://raw.githubusercontent.com/ClementeCEO/CNT/refs/heads/main/canales.json';
+export const TWITCH_PARENT = 'ClementeCEO.github.io'
 
 export const CLASE_CSS_BOTON_PRIMARIO = 'btn-indigo';
 export const CLASE_CSS_BOTON_SECUNDARIO = 'btn-dark-subtle'
